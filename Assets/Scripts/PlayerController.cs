fa77d6884b2a91160c12c8b2d13a1e7218cc99aa
V.Pavlov <v.pavlov@carx-tech.com>
2023-11-03 18:45:54 +0300

    Refactor PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,23 +8,17 @@ namespace ShadowChimera
 {
 	public class PlayerController : MonoBehaviour
 	{
+		[SerializeField] private Character m_character;
 		[SerializeField] private InputActionAsset m_inputActionAsset;
-		[SerializeField] private CharacterController m_characterController;
 		[SerializeField] private Transform m_cameraTarget;
-		[SerializeField] private Transform m_cameraTransform;
-		
-		[SerializeField] private float m_rotationSmoothTime = 0.12f;
-		[SerializeField] private float m_speedChangeRate = 10f;
-		[SerializeField] private float m_moveSpeed = 5f;
-		[SerializeField] private float m_sprintSpeed = 10f;
+		[SerializeField] private Transform m_cameraTransform;		
 		[SerializeField] private float m_speedRotation = 200f;
 		[SerializeField] private float m_topClamp = 70f;
 		[SerializeField] private float m_bottomClamp = -9f;
 
+
 		private float m_cameraTargetYaw;
 		private float m_cameraTargetPitch;
-		private float m_rotationVelocity;
-		private float m_targetRotation;
 
 		//Input
 		private InputActionMap m_playerMap;
@@ -75,55 +69,8 @@ namespace ShadowChimera
 
 		private void Move(Vector2 move, bool isSprint)
 		{
-			float targetSpeed = 0f;
-			float speed = 0;
-			float inputMagnitude = move.magnitude;
-
-			if (inputMagnitude != 0)
-			{
-				targetSpeed = isSprint ? m_sprintSpeed : m_moveSpeed;
-			}
-
-			var characterVelocity = m_characterController.velocity;
-			float currentHorizontalSpeed = new Vector3(characterVelocity.x, 0f, characterVelocity.z).magnitude;
-
-			const float speedOffset = 0.1f;
-
-			if (currentHorizontalSpeed < targetSpeed - speedOffset ||
-			    currentHorizontalSpeed > targetSpeed + speedOffset)
-			{
-				speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * m_speedChangeRate);
-
-				// round speed to 3 decimal places
-				speed = Mathf.Round(speed * 1000f) / 1000f;
-			}
-			else
-			{
-				speed = targetSpeed;
-			}
-			
-			var targetTr = m_characterController.transform;
-			
-			
-			if (inputMagnitude != 0f)
-			{
-				Vector3 inputDirection = new Vector3(move.x, 0f, move.y).normalized;
-				
-				m_targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + m_cameraTransform.eulerAngles.y;
-				
-				float rotation = Mathf.SmoothDampAngle(targetTr.eulerAngles.y, m_targetRotation, ref m_rotationVelocity, m_rotationSmoothTime);
-
-				targetTr.rotation = Quaternion.Euler(0f, rotation, 0f);
-			}
-			
-
-			Vector3 targetDirection = Quaternion.Euler(0f, m_targetRotation, 0f) * Vector3.forward;
-			Vector3 vertical = new Vector3(0f, Physics.gravity.y * Time.deltaTime, 0f);
-			Vector3 horizontal = targetDirection.normalized * (speed * Time.deltaTime);
-			m_characterController.Move(horizontal + vertical);
-		}
-
-		
+			m_character.Move(move, isSprint, m_cameraTransform.eulerAngles.y);
+		}		
 
 		private void CameraRotation(Vector2 look)
 		{
@@ -140,7 +87,7 @@ namespace ShadowChimera
 			m_cameraTargetYaw = ClampAngle(m_cameraTargetYaw, float.MinValue, float.MaxValue);
 			m_cameraTargetPitch = ClampAngle(m_cameraTargetPitch, m_bottomClamp, m_topClamp);
 
-			m_cameraTarget.rotation = Quaternion.Euler(m_cameraTargetPitch, m_cameraTargetYaw, 0f);
+			m_character.Look(Quaternion.Euler(m_cameraTargetPitch, m_cameraTargetYaw, 0f));
 		}
 
 		private static float ClampAngle(float angle, float min, float max)
