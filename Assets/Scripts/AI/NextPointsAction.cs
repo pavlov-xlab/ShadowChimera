7a4b9819517e8003c4ad274f1b3dbc071befb26b
V.Pavlov <v.pavlov@carx-tech.com>
2023-11-10 17:19:46 +0300

    Патруль

---
+++ Assets/Scripts/AI/NextPointsAction.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TheKiwiCoder;
+using UnityEngine;
+
+namespace ShadowChimera
+{
+	public class NextPointsAction : ActionNode
+	{
+		private List<Vector3> m_points = new();
+		private int m_index = 0;
+
+		protected override void OnStart()
+		{
+			var pointsContainer = context.gameObject.GetComponent<PointsContainer>();
+			if (pointsContainer == null)
+			{
+				Debug.LogError("PointsContainer not found!");
+				return;
+			}
+			
+			m_points = pointsContainer.GetPoints();
+		}
+
+		protected override void OnStop()
+		{
+			
+		}
+
+		protected override State OnUpdate()
+		{
+			if (m_points.Count == 0)
+			{
+				return State.Failure;
+			}
+
+			if (m_index >= m_points.Count)
+			{
+				m_index = 0;
+			} 
+
+			blackboard.moveToPosition = m_points[m_index++];
+
+			return State.Success;
+		}
+	}
+}
